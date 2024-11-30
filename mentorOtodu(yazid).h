89d3f864c0210ff06185f827633cb5ddb8d29107
Yazid Ilmi Ihsan <151598845+yazidtamvan@users.noreply.github.com>
2024-12-01 00:21:41 +0700

    Update and rename mentorotodu(yazid).cpp to mentorOtodu(yazid).h

---
+++ mentorOtodu(yazid).h
@@ -0,0 +1,28 @@
+void mentorOtodu() {
+    int choice;
+
+    do {
+        cout << "\nMenu:" << endl;
+        cout << "1. Siswa Anda" << endl;
+        cout << "2. Antrian Siswa Anda" << endl;
+        cout << "3. Logout" << endl;
+        cout << "Pilih (1-3): ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                siswaAnda();
+                break;
+            case 2:
+                antrianSiswaAnda();
+                break;
+            case 3:
+                logout();
+                return; // Kembali ke fungsi pemanggil (main)
+            default:
+                cout << "\nPilihan tidak valid. Silakan coba lagi." << endl;
+                break;
+        }
+    } while (true);
+}
+
